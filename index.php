16384f6b605d65ee011de6b7fab1a8bf6cb916fa
Artwolf <jonty_wolf@hotmail.com>
2018-02-22 13:47:39 +0100

    perform height by duration completed

--- index.php
+++ index.php
@@ -25,14 +25,14 @@
         <div class="auto text-center cell">Podium 4</div>
     </div>
     <div class="grid-x">
-        <div class="small-1 text-center cell">00:05</div>
-        <div class="auto cell">
+        <div class="small-1 text-center time-cell cell">00:05</div>
+        <div class="auto timetable-perform-cell cell">
             <div class="timetable-perform-wrapper sortable">
                 <div class="timetable-perform">
                     <b>Foo</b>
                     <p>Starttijd: <span class="startTime">01:00</span></p>
-                    <p>duratie: <span class="duration">00:20</span></p>
-                    <p>Eindtijd:<span class="endTime">01:15</span></p>
+                    <p>duratie: <span class="duration">00:25</span></p>
+                    <p>Eindtijd: <span class="endTime">01:15</span></p>
                 </div>
             </div>
         </div>
--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-$(document).ready(function () {
-    console.log($(".cell").css("height"));
-    console.log($(".cell")[1].css("width"));
+function getDuration(duration) {
+    var pieces = duration.split(":");
+
+    var hours = parseInt(pieces[0]),
+        minutes = parseInt(pieces[1]),
+        totalMinutes = hours * 60 + minutes;
+
+    return totalMinutes;
+}
+
+var duration = $('.duration').text();
+duration = getDuration(duration);
+
+var height = $(".time-cell").css("height")
+height = height.replace('px', '');
+
+var performHeight = height * (duration / 5);
+
+
+$('.timetable-perform').css({
+    'height': performHeight,
+    'background-color': 'blue'
 });
+
+$('.grid-container').fadeIn('slow');
