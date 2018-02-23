e70321bf528bc90f7eb12f854617b988dae13750
Artwolf <jonty_wolf@hotmail.com>
2018-02-23 09:55:16 +0100

    Made Timetable perform wrapper

--- index.php
+++ index.php
@@ -17,23 +17,21 @@
 <div class="grid-container full">
     <div class="timetable-perform-wrapper">
     </div>
-    <div class="grid-x">
+    <div class="grid-x podium-row">
         <div class="small-1 cell"></div>
-        <div class="auto text-center cell">Podium 1</div>
+        <div id="podium" class="auto podium text-center cell">Podium 1</div>
         <div class="auto text-center cell">Podium 2</div>
         <div class="auto text-center cell">Podium 3</div>
         <div class="auto text-center cell">Podium 4</div>
     </div>
     <div class="grid-x">
         <div class="small-1 text-center time-cell cell">00:05</div>
-        <div class="auto timetable-perform-cell cell">
-            <div class="timetable-perform-wrapper sortable">
-                <div class="timetable-perform">
-                    <b>Foo</b>
-                    <p>Starttijd: <span class="startTime">01:00</span></p>
-                    <p>duratie: <span class="duration">00:25</span></p>
-                    <p>Eindtijd: <span class="endTime">01:15</span></p>
-                </div>
+        <div class="auto timetable-perform-cell sortable cell">
+            <div class="timetable-perform">
+                <b>Foo</b>
+                <p>Starttijd: <span class="startTime">01:00</span></p>
+                <p>duratie: <span class="duration">00:25</span></p>
+                <p>Eindtijd: <span class="endTime">01:15</span></p>
             </div>
         </div>
         <div class="auto cell">2</div>
--- main.js
+++ main.js
@@ -1,25 +1,48 @@
-function getDuration(duration) {
-    var pieces = duration.split(":");
+$(document).ready(function () {
+    function getDuration(duration) {
+        var pieces = duration.split(":");
 
-    var hours = parseInt(pieces[0]),
-        minutes = parseInt(pieces[1]),
-        totalMinutes = hours * 60 + minutes;
+        var hours = parseInt(pieces[0]),
+            minutes = parseInt(pieces[1]),
+            totalMinutes = hours * 60 + minutes;
 
-    return totalMinutes;
-}
+        return totalMinutes;
+    }
 
-var duration = $('.duration').text();
-duration = getDuration(duration);
+    function calculateTimetable(){
+        var duration = $('.duration').text();
+        duration = getDuration(duration);
 
-var height = $(".time-cell").css("height")
-height = height.replace('px', '');
+        var height = $(".time-cell").height();
+        var width = $("#podium").width();
+        var timeCellWidth = $('.time-cell').width()
 
-var performHeight = height * (duration / 5);
+        var wrapperRowCount = $('.grid-container').children('.grid-x').length - 1;
+        var wrapperColumnCount = $('.podium-row').children('.auto').length;
 
+        var wrapperWidth = wrapperColumnCount * width;
+        var wrapperHeight = wrapperRowCount * height;
 
-$('.timetable-perform').css({
-    'height': performHeight,
-    'background-color': 'blue'
+        var performHeight = height * (duration / 5);
+
+        $('.timetable-perform-wrapper').css({
+            'height': wrapperHeight,
+            'width':  wrapperWidth,
+            'z-index': 0,
+            'margin-top': height + 'px',
+            'margin-left': timeCellWidth + 'px',
+            'position': 'absolute'
+        });
+
+        $('.timetable-perform').css({
+            'height': performHeight,
+            'background-color': 'blue'
+        });
+    }
+
+    $(window).resize(function() {
+        calculateTimetable();
+    });
+
+    calculateTimetable();
 });
-
-$('.grid-container').fadeIn('slow');
