2d6d43ff9f96e74c7970357383571c1d686f7695
Artwolf <jonty_wolf@hotmail.com>
2018-02-26 13:18:59 +0100

    Finished everything

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="foundation/css/foundation.min.css">
     <link rel="stylesheet" href="jquery/jquery-ui.min.css">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.6/css/all.css">
     <title>Title</title>
 </head>
 <body>
@@ -19,132 +20,213 @@
     </div>
     <div class="grid-x podium-row">
         <div class="small-1 cell"></div>
-        <div id="podium" class="auto podium text-center cell">Podium 1</div>
-        <div class="auto text-center cell">Podium 2</div>
-        <div class="auto text-center cell">Podium 3</div>
-        <div class="auto text-center cell">Podium 4</div>
+        <div id="podium" class="auto podium text-center cell"><b>Podium 1</b></div>
+        <div class="auto text-center cell"><b>Podium 2</b></div>
+        <div class="auto text-center cell"><b>Podium 3</b></div>
+        <div class="auto text-center cell"><b>Podium 4</b></div>
     </div>
     <div class="grid-x">
         <div class="small-1 text-center time-cell cell">00:05</div>
         <div class="auto timetable-perform-cell sortable cell">
             <div class="timetable-perform">
-                <b>Foo</b>
-                <p>Starttijd: <span class="startTime">01:00</span></p>
-                <p>duratie: <span class="duration">00:25</span></p>
-                <p>Eindtijd: <span class="endTime">01:15</span></p>
+                <div class="timetable-perform-container">
+                    <b>Foo</b>
+                    <i class="far perform-delete fa-times-circle"></i>
+                    <i class="far perform-edit fa-edit"></i>
+                    <p>Starttijd: <span class="startTime">01:00</span></p>
+                    <p>Tijdsduur: <span class="duration">00:55</span></p>
+                    <p>Eindtijd: <span class="endTime">01:15</span></p>
+                </div>
+
             </div>
         </div>
-        <div class="auto cell">2</div>
-        <div class="auto cell">3</div>
-        <div class="auto cell">4</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
     </div>
     <div class="grid-x">
         <div class="small-1 text-center cell">00:10</div>
-        <div class="auto cell">1</div>
-        <div class="auto cell">2</div>
-        <div class="auto cell">3</div>
-        <div class="auto cell">4</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
     </div>
     <div class="grid-x ">
         <div class="small-1 text-center cell">00:15</div>
-        <div class="auto cell">1</div>
-        <div class="auto cell">2</div>
-        <div class="auto cell">3</div>
-        <div class="auto cell">4</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
     </div>
     <div class="grid-x ">
         <div class="small-1 text-center cell">00:20</div>
-        <div class="auto cell">1</div>
-        <div class="auto cell">2</div>
-        <div class="auto cell">3</div>
-        <div class="auto cell">4</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
     </div>
     <div class="grid-x ">
         <div class="small-1 text-center cell">00:25</div>
-        <div class="auto cell">1</div>
-        <div class="auto cell">2</div>
-        <div class="auto cell">3</div>
-        <div class="auto cell">4</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
     </div>
     <div class="grid-x ">
         <div class="small-1 text-center cell">00:30</div>
-        <div class="auto cell">1</div>
-        <div class="auto cell">2</div>
-        <div class="auto cell">3</div>
-        <div class="auto cell">4</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
     </div>
     <div class="grid-x ">
         <div class="small-1 text-center cell">00:35</div>
-        <div class="auto cell">1</div>
-        <div class="auto cell">2</div>
-        <div class="auto cell">3</div>
-        <div class="auto cell">4</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
     </div>
     <div class="grid-x ">
         <div class="small-1 text-center cell">00:40</div>
-        <div class="auto cell">1</div>
-        <div class="auto cell">2</div>
-        <div class="auto cell">3</div>
-        <div class="auto cell">4</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
     </div>
     <div class="grid-x ">
         <div class="small-1 text-center cell">00:45</div>
-        <div class="auto cell">1</div>
-        <div class="auto cell">2</div>
-        <div class="auto cell">3</div>
-        <div class="auto cell">4</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
     </div>
     <div class="grid-x ">
         <div class="small-1 text-center cell">00:50</div>
-        <div class="auto cell">1</div>
-        <div class="auto cell">2</div>
-        <div class="auto cell">3</div>
-        <div class="auto cell">4</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
     </div>
     <div class="grid-x ">
         <div class="small-1 text-center cell">00:55</div>
-        <div class="auto cell">1</div>
-        <div class="auto cell">2</div>
-        <div class="auto cell">3</div>
-        <div class="auto cell">4</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
     </div>
     <div class="grid-x ">
         <div class="small-1 text-center cell">01:00</div>
-        <div class="auto cell">1</div>
-        <div class="auto cell">2</div>
-        <div class="auto cell">3</div>
-        <div class="auto cell">4</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
     </div>
     <div class="grid-x ">
         <div class="small-1 text-center cell">01:05</div>
-        <div class="auto cell">1</div>
-        <div class="auto cell">2</div>
-        <div class="auto cell">3</div>
-        <div class="auto cell">4</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
     </div>
     <div class="grid-x ">
         <div class="small-1 text-center cell">01:10</div>
-        <div class="auto cell">1</div>
-        <div class="auto cell">2</div>
-        <div class="auto cell">3</div>
-        <div class="auto cell">4</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
     </div>
     <div class="grid-x ">
         <div class="small-1 text-center cell">01:15</div>
-        <div class="auto cell">1</div>
-        <div class="auto cell">2</div>
-        <div class="auto cell">3</div>
-        <div class="auto cell">4</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
     </div>
     <div class="grid-x ">
         <div class="small-1 text-center cell">01:20</div>
-        <div class="auto cell">1</div>
-        <div class="auto cell">2</div>
-        <div class="auto cell">3</div>
-        <div class="auto cell">4</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
+    </div>
+    <div class="grid-x ">
+        <div class="small-1 text-center cell">01:25</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
+    </div>
+    <div class="grid-x ">
+        <div class="small-1 text-center cell">01:30</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
+    </div>
+    <div class="grid-x ">
+        <div class="small-1 text-center cell">01:35</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
+    </div>
+    <div class="grid-x ">
+        <div class="small-1 text-center cell">01:40</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
+    </div>
+    <div class="grid-x ">
+        <div class="small-1 text-center cell">01:45</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
+    </div>
+    <div class="grid-x ">
+        <div class="small-1 text-center cell">01:50</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
+    </div>
+    <div class="grid-x ">
+        <div class="small-1 text-center cell">01:55</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
+    </div>
+    <div class="grid-x ">
+        <div class="small-1 text-center cell">02:00</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
+    </div>
+    <div class="grid-x ">
+        <div class="small-1 text-center cell">02:05</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
+    </div>
+    <div class="grid-x ">
+        <div class="small-1 text-center cell">02:10</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
+    </div>
+    <div class="grid-x ">
+        <div class="small-1 text-center cell">02:15</div>
+        <div class="auto sortable cell">1</div>
+        <div class="auto sortable cell">2</div>
+        <div class="auto sortable cell">3</div>
+        <div class="auto sortable cell">4</div>
     </div>
 </div>
-
 <script src="jquery/external/jquery/jquery.js"></script>
 <script src="foundation/js/vendor/foundation.min.js"></script>
 <script src="jquery/jquery-ui.min.js"></script>
--- main.js
+++ main.js
@@ -1,4 +1,52 @@
 $(document).ready(function () {
+    function changeTimeFields(dragged, duration){
+        var startTime = changeStartTime(dragged);
+
+        changeEndTime(startTime, duration, dragged);
+    }
+
+    function changeStartTime(dragged){
+        var parent = $(dragged[0]).parent(),
+            timeCell = $(parent).children(":first-child"),
+            time =  $(timeCell).text();
+
+        $(".startTime").html(time);
+
+        return time;
+    }
+
+    function timeWithDuration(startTime, duratie){
+        var pieces = startTime.split(":");
+
+        var startTimeHours = parseInt(pieces[0]),
+            startTimeMinutes = parseInt(pieces[1]);
+
+        var totalMinutes = startTimeHours * 60 + startTimeMinutes + duratie;
+        var hours = Math.floor(totalMinutes / 60).toString();
+        var minutes = totalMinutes % 60;
+        minutes = minutes.toString();
+
+        if(hours.length < 2){
+            hours = "0" + hours;
+        }
+
+        if(minutes.length < 2){
+            minutes = "0" + minutes;
+        }
+
+        var endTime = hours + ':' + minutes;
+
+        return endTime;
+    }
+
+    function changeEndTime(startTime, duratie) {
+        var endTime = timeWithDuration(startTime, duratie);
+
+        $(".endTime").html(endTime);
+
+        calculateTimetable();
+    }
+
     function getDuration(duration) {
         var pieces = duration.split(":");
 
@@ -9,12 +57,27 @@ $(document).ready(function () {
         return totalMinutes;
     }
 
+    function swap(dragged, dropped) {
+        var draggedPrev = $(dragged).prev();
+        var droppedPrev = $(dropped).prev();
+
+        $(draggedPrev).after(dropped);
+        $(droppedPrev).after(dragged);
+
+        $(dragged).css({
+            'top': '0px',
+            'left': '0px'
+        });
+    }
+
     function calculateTimetable(){
+        $('.timetable-perform').fadeIn('fast');
+
         var duration = $('.duration').text();
         duration = getDuration(duration);
 
         var height = $(".time-cell").height();
-        var width = $("#podium").width();
+        var width = $('.podium-row').children(':nth-child(2)').width();
         var timeCellWidth = $('.time-cell').width()
 
         var wrapperRowCount = $('.grid-container').children('.grid-x').length - 1;
@@ -29,14 +92,31 @@ $(document).ready(function () {
             'height': wrapperHeight,
             'width':  wrapperWidth,
             'z-index': 0,
-            'margin-top': height + 'px',
-            'margin-left': timeCellWidth + 'px',
+            'margin-top': (height -1)  + 'px',
+            'margin-left': (timeCellWidth - 1) + 'px',
             'position': 'absolute'
         });
 
         $('.timetable-perform').css({
             'height': performHeight,
-            'background-color': 'blue'
+            'width': width + 2
+        });
+
+        $(".timetable-perform-cell").draggable({
+            cursor: 'move',
+            containment: $(".timetable-perform-wrapper"),
+            grid: [width + 0.75, height]
+        });
+
+        $('.sortable').droppable({
+            accept: '.sortable',
+            drop: function (event, ui) {
+                var dragged = ui.draggable,
+                    dropped = $(this);
+
+                swap(dragged, dropped);
+                changeTimeFields(dragged, duration);
+            }
         });
     }
 
